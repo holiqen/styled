c806e9272b180509fb8654ed308c892c424a8e1b
Artem G <artemg@whale.digital>
2020-01-14 01:01:52 +0300

    Updates

---
+++ precache-manifest.6c7d25cc7882eb7541b0198f5903610a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "367bae49b42c6f1d29183f059b1414cb",
+    "url": "/styled/index.html"
+  },
+  {
+    "revision": "8c94eb16b913e8f879ff",
+    "url": "/styled/static/js/2.1da6a2f9.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/styled/static/js/2.1da6a2f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "98dc716dba9c225090f4",
+    "url": "/styled/static/js/main.47b377ac.chunk.js"
+  },
+  {
+    "revision": "b037ae7cb75e3aec4650",
+    "url": "/styled/static/js/runtime-main.f16f924f.js"
+  },
+  {
+    "revision": "0e394d2533b6a364eef64e82e309feb8",
+    "url": "/styled/static/media/aboutBcg.0e394d25.jpeg"
+  },
+  {
+    "revision": "3811bd6a3c0a20b9e3bc612d8f4fd2b5",
+    "url": "/styled/static/media/homeBcg.3811bd6a.jpg"
+  },
+  {
+    "revision": "7577289d7512b6908b107a7a9658717f",
+    "url": "/styled/static/media/room1.7577289d.jpeg"
+  },
+  {
+    "revision": "4e32e8c29c55148da633b6d83654796b",
+    "url": "/styled/static/media/room2.4e32e8c2.jpeg"
+  },
+  {
+    "revision": "37780ac0f97ce693235c6dd6daec1508",
+    "url": "/styled/static/media/room3.37780ac0.jpeg"
+  }
+]);
